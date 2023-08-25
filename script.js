31e4464893b4c8b16ff3225af7946bd47103b130
BhavyaCPatel <bhavyaaes@gmail.com>
2023-08-26 00:03:08 +0530

    Final commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const videoElement = document.getElementById('camera-feed');
+const captureButton = document.getElementById('capture-btn');
+const saveButton = document.getElementById('save-btn');
+const canvasElement = document.getElementById('photo-canvas');
+
+// Get user media and set up video stream
+navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function (stream) {
+        videoElement.srcObject = stream;
+    })
+    .catch(function (error) {
+        console.error('Error accessing the camera:', error);
+    });
+
+// Capture a photo from the video stream
+captureButton.addEventListener('click', function () {
+    canvasElement.width = videoElement.videoWidth;
+    canvasElement.height = videoElement.videoHeight;
+    canvasElement.getContext('2d').drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height);
+    canvasElement.style.display = 'block';
+});
+
+// Save the captured photo to local storage
+// saveButton.addEventListener('click', function () {
+//     if (canvasElement.style.display === 'block') {
+//         const imageDataURL = canvasElement.toDataURL('image/png');
+//         localStorage.setItem('capturedPhoto', imageDataURL);
+//         alert('Photo saved to local storage.');
+//         canvasElement.style.display = 'none';
+//     } else {
+//         alert('No photo to save. Capture a photo first.');
+//     }
+// });
